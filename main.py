f7ac556d1b84066263af1d2f879a87bbfa96e4f5
Carlos Petroski <carloseduardopetroski486@gmail.com>
2025-04-13 23:24:15 -0300

    teste-docker

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ async def funcaoteste():
     return {"message": True, "num_aleatorio": random.randint(0, 1000)}
 
 @app.get("/teste2")
-async def funcaoteste():
+async def funcaoteste2():
     return {"message": "Teste para o CI/CD"}
 
